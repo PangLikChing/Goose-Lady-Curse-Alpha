1a63e4aa6ab8e2aafd981ec41858941f0418cfc7
PangLikChing <panglikching@gmail.com>
2022-05-18 01:09:30 -0400

    Simple Drag and Drop done. Swap data tmr

---
+++ Assets/Harvey/Scripts/Game/Drag and Drop/DragDrop.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// This script is responsible for handling the drag and drop of an item in the inventory
+/// </summary>
+
+[RequireComponent(typeof(CanvasGroup))]
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Canvas canvas;
+
+    void Start()
+    {
+        // Initialize
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+
+        // Hard code this one, fix later
+        canvas = transform.parent.parent.parent.parent.GetComponent<Canvas>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Make it so that it is not interactable with raycast when dragging
+        canvasGroup.blocksRaycasts = false;
+
+        // Change the parent to the canvas so that it will not be blocked by other slots
+        transform.parent = canvas.transform;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Move the dragged object with mouse
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Reset blockRaycasts
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+}
--- Assets/Harvey/Scripts/Game/InventorySlot.cs
+++ Assets/Harvey/Scripts/Game/InventorySlot.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public ItemSlot itemSlot;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // If something is being dragged into the slot
+        if (eventData.pointerDrag != null)
+        {
+            // Set it to my child
+            eventData.pointerDrag.transform.SetParent(transform);
+
+            // Snap that item into the slot
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        }
+    }
 }
