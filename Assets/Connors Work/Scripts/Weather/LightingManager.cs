4cc467a85a96dc3898e369363c8deda6479582db
Connor Brennan <brenncon@sheridancollege.ca>
2022-05-20 13:16:43 -0400

    Added Rain, Snow, and a Night and Day Cycle

---
+++ Assets/Connors Work/Scripts/Weather/LightingManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    //Scene References
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    //Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    public bool DemoRun = false;
+    public float WaitTimeBeforeChangeLight;
+    private float Timer = 0;
+    public float AmountToChangeLightBy;
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            if(DemoRun == true)
+            {
+                //(Replace with a reference to the game time)
+                TimeOfDay += Time.deltaTime;
+                TimeOfDay %= 24; //Modulus to ensure always between 0-24
+                UpdateLighting(TimeOfDay / 24f);
+            }
+            else
+            {
+                if(Timer >= WaitTimeBeforeChangeLight)
+                {
+                    Timer = 0;
+                    TimeOfDay += AmountToChangeLightBy;
+                    TimeOfDay %= 24; //Modulus to ensure always between 0-24
+                    UpdateLighting(TimeOfDay / 24f);
+                }
+                else
+                {
+                    Timer += Time.deltaTime;
+                }
+            }
+            
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+
+    private void UpdateLighting(float timePercent)
+    {
+        //Set ambient and fog
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+        //If the directional light is set then rotate and set it's color
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+
+
+    //Try to find a directional light to use if we haven't set one
+    private void OnValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        //Search for lighting tab sun
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        //Search scene for light that fits criteria (directional)
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+
+}
