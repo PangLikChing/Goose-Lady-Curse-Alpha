a81d98546884815ba98a0e112db6a4a31ac47587
Qiao <qiaokuanghua@gmail.com>
2022-05-20 00:34:12 -0400

    The avatar movement work with current setting in the inspector However, it still have the circling behavior if change the avatar speed Might break if we change avatar.

--- Assets/Game/Scripts/Player/Actuators/AvatarLocomotion.cs
+++ Assets/Game/Scripts/Player/Actuators/AvatarLocomotion.cs
@@ -7,16 +7,19 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class AvatarLocomotion : MonoBehaviour
 {
-    public Transform target;
     public float speedDampeningTime = 3;
     public float speedDeadZone = 0.001f;
     public float angularDampeningTime = 5.0f;
     public float angularDeadZone = 10.0f;
-    public float stoppingDistance = 0.2f;
-    public bool useRootMotion;
+    public float speedMultiplier = 1;
+    public float brakingDistance = 5;
 
+#if UNITY_EDITOR
+    public float agentSpeed;
+#endif
 
     private readonly int SpeedParameter = Animator.StringToHash("speed");
+    private readonly int SpeedMultiplierParameter = Animator.StringToHash("speed multiplier");
     private AnimationListener animationListener;
     private NavMeshAgent agent;
     private Animator avatarAnimator;
@@ -36,51 +39,56 @@ public class AvatarLocomotion : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //agent.speed = 1;
+        agent.angularSpeed = 0;
+
         if (hasAnimationListner)
         {
             animationListener.OnAnimatorMoveEvent += OnAnimatorMove;
+        }
+        if (hasAnimator)
+        {
+            avatarAnimator.SetFloat(SpeedMultiplierParameter, speedMultiplier);
         }
     }
 
     private void OnAnimatorMove()
     {
-        if (useRootMotion)
-        {
-            agent.velocity = avatarAnimator.deltaPosition / Time.deltaTime;
-        }
+        agent.velocity = avatarAnimator.deltaPosition / Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+#if UNITY_EDITOR
+        agentSpeed = agent.velocity.magnitude;
         if (hasAnimator)
         {
-            if (useRootMotion)
+            avatarAnimator.SetFloat(SpeedMultiplierParameter, speedMultiplier);
+        }
+#endif
+        if (hasAnimator)
+        {
+            if (agent.remainingDistance < agent.stoppingDistance && agent.pathStatus == NavMeshPathStatus.PathComplete)
             {
-                if (agent.remainingDistance < agent.radius)
-                {
-                    targetSpeed = 0;
-                }
-                else
-                {
-                    targetSpeed = agent.desiredVelocity.magnitude;
-                }
+                
+                targetSpeed = 0;
+                //agent.velocity = Vector3.zero;
+            }
+            else
+            {
+                targetSpeed = agent.desiredVelocity.magnitude;
+            }
 
-                if (Mathf.Abs(currentSpeed - targetSpeed) > speedDeadZone)
-                {
-                    currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, speedDampeningTime * Time.deltaTime);
-                    //currentSpeed = Vector3.Lerp(agent.velocity,agent.desiredVelocity, speedDampeningTime * Time.deltaTime).magnitude;
-                }
-                else
-                {
-                    currentSpeed = targetSpeed;
-                }
-                avatarAnimator.SetFloat(SpeedParameter, currentSpeed);
-            }
-            else 
+            if (Mathf.Abs(currentSpeed - targetSpeed) > speedDeadZone)
             {
-                avatarAnimator.SetFloat(SpeedParameter, agent.velocity.magnitude);
+                currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, speedDampeningTime * Time.deltaTime);
             }
+            else
+            {
+                currentSpeed = targetSpeed;
+            }
+            avatarAnimator.SetFloat(SpeedParameter, currentSpeed);
 
             if (agent.remainingDistance > agent.radius)
             {
@@ -88,27 +96,19 @@ public class AvatarLocomotion : MonoBehaviour
                 if (Mathf.Abs(angle) <= angularDeadZone)
                 {
                     transform.LookAt(transform.position + agent.desiredVelocity);
+
                 }
                 else
                 {
                     transform.rotation = Quaternion.Lerp(transform.rotation,
                                                          Quaternion.LookRotation(agent.desiredVelocity),
                                                          Time.deltaTime * angularDampeningTime);
-                }
-            }
-        }
-        //if (target != null)
-        //{
-        //    MoveToPoint(target.position);
-        //    FaceTarget();
-        //}
-    }
 
-    void FaceTarget()
-    {
-        Vector3 direction = (target.position - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+                }
+
+            }
+
+        }
     }
 
     public void MoveToPoint(Vector3 point)
---
+++ Assets/Game/Scripts/Player/Controller/Camera/LockCameraHeight.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// An add-on module for Cinemachine Virtual Camera that locks the camera's Z co-ordinate
+/// </summary>
+[ExecuteInEditMode]
+[SaveDuringPlay]
+[AddComponentMenu("")] // Hide in menu
+public class LockCameraHeight : CinemachineExtension
+{
+    [Tooltip("Lock the camera's Height to this value")]
+    public float cameraHeight = 13;
+
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            pos.y = cameraHeight;
+            state.RawPosition = pos;
+        }
+    }
+}
